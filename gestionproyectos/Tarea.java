435b15d3eb9f5f523fadab2c11a5a5400daff699
hpsopastech <andres.ar.tech@gmail.com>
2026-09-14 15:15:01 -0500

    se soluciono error con codigo en el paquete original

--- gestionproyectos/Tarea.java
+++ gestionproyectos/Tarea.java
@@ -3,6 +3,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package TallerPOOMiniAppGestionDeProyectos.gestionproyectos;
+
+
+
 /**
  *
  * @author anran
